adf90e6a7d3f6a14db67e92c5b48e7072e24fd0c
User <nevo.krien@gmail.com>
2023-07-12 13:00:36 +0300

    added input preperation. need to add semantic distance

--- memory.py
+++ memory.py
@@ -55,7 +55,29 @@ class memory():
         np.save(save_path, {'text': text, 'key': get_embedings(text),'importance': importance,
         	'time':datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
 
-    def search(self, place, depth=-1, date_range=None, importance_threshold=None, keyword=None, only_created=False):
+
+    def change_importance(self, heading: str,importance :Union[int,float], place='history'):
+        path = join(self.path, place, heading)
+        assert '.' not in path #this makes sure the ai dosent access the external file system
+        x=np.load(path+'.npy', allow_pickle=True).item()
+        #print(x)
+        x['importance']=importance 
+        np.save(path+'.npy',x)
+        
+
+    def remove_memory(self, heading: str, place='history'):
+        path = join(self.path, place, heading)
+        assert '.' not in path #this makes sure the ai dosent access the external file system
+        os.remove(path+'.npy')
+            
+    
+    def remove_folder(self, folder:  str, place=''):
+        path = join(self.path, place, folder)
+        assert '.' not in path #this makes sure the ai dosent access the external file system
+        assert not path in self.get_base_folders() 
+        rmtree(path)
+    
+    def _search(self, place, depth=-1, date_range=None, importance_threshold=None, keyword=None, only_created=False):
         path = join(self.path, place)
         assert exists(path)
         return self._recursive_search(path, depth, date_range, importance_threshold, keyword, only_created)
@@ -86,27 +108,37 @@ class memory():
                 results.extend(self._recursive_search(entry.path, depth - 1, date_range, importance_threshold, keyword, only_created))
         return results
 
-    def change_importance(self, heading: str,importance :Union[int,float], place='history'):
-        path = join(self.path, place, heading)
-        assert '.' not in path #this makes sure the ai dosent access the external file system
-        x=np.load(path+'.npy', allow_pickle=True).item()
-        #print(x)
-        x['importance']=importance 
-        np.save(path+'.npy',x)
-        
+    def search(self, place, depth=-1, date_range=None, importance_threshold=None, keyword=None, only_created=False, fields=('text',)):
+        raw_results = self._search(place, depth, date_range, importance_threshold, keyword, only_created)
+        tree = {}
+        data = []
 
-    def remove_memory(self, heading: str, place='history'):
-        path = join(self.path, place, heading)
-        assert '.' not in path #this makes sure the ai dosent access the external file system
-        os.remove(path+'.npy')
-            
-    
-    def remove_folder(self, folder:  str, place=''):
-        path = join(self.path, place, folder)
-        assert '.' not in path #this makes sure the ai dosent access the external file system
-        assert not path in self.get_base_folders() 
-        rmtree(path)
-      
+        for dirpath, filename, contents in raw_results:
+            relative_path = os.path.relpath(dirpath, self.path)
+            path_parts = relative_path.split(os.path.sep)
+            cursor = tree
+            for part in path_parts:
+                if part not in cursor:
+                    cursor[part] = {}
+                cursor = cursor[part]
+            info = {field: contents[field] for field in fields}
+            cursor[filename] = info
+            data.append((dirpath, filename, info))
+
+        def pretty_print_tree(tree_dict, indent=''):
+            lines = []
+            for key, value in tree_dict.items():
+                if isinstance(value, dict):
+                    lines.append(f"{indent}{key}:")
+                    lines.extend(pretty_print_tree(value, indent=indent + '\t'))
+                else:
+                    lines.append(f"{indent}{key}: {value}")
+            return lines
+
+        lines = pretty_print_tree(tree)
+        return '\n'.join(lines), data
+
+     
 if __name__ == '__main__':
     if exists('lol'):
     	rmtree('lol')
@@ -116,18 +148,20 @@ if __name__ == '__main__':
     x.add_memory('mem','first',1)
     x.add_memory('mem','i added u',10,place=join('history','dirr'))
     memory('lol').change_importance('mem',2,place=join('history','dirr'))
-    print(memory('lol').search('history',2,importance_threshold=2))
+    print(memory('lol')._search('history',2,importance_threshold=2))
 
     print('\n\n\n')
     x.add_memory('mem2','first',1)
     x.add_memory('stuff','first',1)
     x.remove_memory('mem', place=join('history','dirr'))
-    x.remove_folder('dirr', 'history')
+    #x.remove_folder('dirr', 'history')
     x.add_folder('check','discribing this folder',place="")
-    print(memory('lol').search('history', 1,keyword='mem'))
+    print(memory('lol')._search('history', 1,keyword='mem'))
 
     print('\n\n\n')
 
     x.add_folder('check 2', 'history')
     x.add_folder('check','discribing this folder',place="")
-    print(memory('lol').search('', 2,only_created=True,))
+    tree,data=memory('lol').search('', -1,only_created=True,)
+    print(tree)
+    print(data)
