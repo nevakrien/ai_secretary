17b2181735e3fc8eccbf55537b569ae1b7f27996
User <nevo.krien@gmail.com>
2023-07-15 15:34:44 +0300

    all but viewing tho pruning needs some work

--- memory.py
+++ memory.py
@@ -1,191 +1,133 @@
-import numpy as np
+import json
 import os
-from shutil import rmtree 
+from os.path import join,exists,getctime
+from datetime import datetime
+import time
 
-from datetime import datetime  
-from os.path import join,exists
+from typing import Optional, Dict ,Union
 
-from typing import Union
+from utills import write_int_to_file,read_int_from_file,read_last_line 
 
-#this are just for the test
-import hashlib
-
-def get_embedings(input_string, vector_length=7):
-    hash_object = hashlib.sha256(input_string.encode())
-    hex_dig = hash_object.hexdigest()
-    seed = int(hex_dig, 16) % (2**32 - 1)  # Convert to integer first, then apply modulo
-    np.random.seed(seed)
-    vector = np.random.random(size=vector_length)
-    return vector
-
-
-class memory():
-    def __init__(self, path):
-        assert exists(path)
-        self.path = path 
-    
-    @staticmethod 
-    def make_new(path):    
-        os.makedirs(path)
-        for f in memory.get_base_folders():
-        	os.mkdir(join(path,f))
-        return memory(path)
-    
-    @staticmethod  
-    def get_base_folders():
-    	return ['notes','goals','history']
-
-    def add_folder(self, heading: str, text: str, place='history'):
-        path = join(self.path, place)
-        assert exists(path)
-        assert len(heading) < 100
-        new_folder_path = join(path, heading)
-
-        assert '.' not in new_folder_path #this makes sure the ai dosent access the external file system
-        os.makedirs(new_folder_path, exist_ok=True)
+class MemoryFolder():
+    def __init__(self,path,cap=100,new=False):
         
-        self.add_memory('created',text,2,place=join(place,heading))
-
-    def add_memory(self, heading: str, text : str,importance :Union[int,float], place='history'):
-        path = join(self.path, place)
-        assert exists(path)
-        assert len(heading) < 100
-        save_path=join(path, heading)
-        assert '.' not in save_path #this makes sure the ai dosent access the external file system
-        np.save(save_path, {'text': text, 'key': get_embedings(text),'importance': importance,
-        	'time':datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
-
-
-    def change_importance(self, heading: str,importance :Union[int,float], place='history'):
-        path = join(self.path, place, heading)
-        assert '.' not in path #this makes sure the ai dosent access the external file system
-        x=np.load(path+'.npy', allow_pickle=True).item()
-        #print(x)
-        x['importance']=importance 
-        np.save(path+'.npy',x)
+        self.history=join(path,'history')
+        self.curent=join(path,'curent')
+        self.idx_file=join(path,'idx')
+        self.cap=cap
         
-
-    def remove_memory(self, heading: str, place='history'):
-        path = join(self.path, place, heading)
-        assert '.' not in path #this makes sure the ai dosent access the external file system
-        os.remove(path+'.npy')
-            
+        if new:
+            os.mkdir(path)
+            os.mkdir(self.history)
+            os.mkdir(self.curent)
+            write_int_to_file(self.idx_file,0)
     
-    def remove_folder(self, folder:  str, place=''):
-        path = join(self.path, place, folder)
-        assert '.' not in path #this makes sure the ai dosent access the external file system
-        assert not path in self.get_base_folders() 
-        rmtree(path)
+    def get_count(self):
+        return read_int_from_file(self.idx_file) 
+    def _increment_count(self): 
+        count=self.get_count()+1
+        write_int_to_file(self.idx_file,count) 
     
-    def _search(self, place, depth=-1, date_range=None, importance_threshold=None, keyword=None, only_created=False):
-        path = join(self.path, place)
-        assert exists(path)
-        return self._recursive_search(path, depth, date_range, importance_threshold, keyword, only_created)
-
-    def _recursive_search(self, path, depth, date_range, importance_threshold, keyword, only_created):
-        results = []
-        if depth == 0:
-            return results
-        for entry in os.scandir(path):
-            if entry.is_file() and entry.name.endswith('.npy'):
-                if only_created and entry.name != 'created.npy':
-                    continue
-                if keyword and keyword not in entry.name:
-                    continue
-                data = np.load(entry.path, allow_pickle=True).item()  # convert numpy array to Python dict
-                mem_date = datetime.strptime(data['time'], '%Y-%m-%d %H:%M:%S')
-                mem_importance = data['importance']
-                if date_range:
-                    start_date = datetime.strptime(date_range[0], '%Y-%m-%d')
-                    end_date = datetime.strptime(date_range[1], '%Y-%m-%d')
-                    if not start_date <= mem_date <= end_date:
-                        continue
-                if importance_threshold:
-                    if mem_importance < importance_threshold:
-                        continue
-                results.append((os.path.dirname(entry.path), entry.name, data))
-            elif entry.is_dir():
-                results.extend(self._recursive_search(entry.path, depth - 1, date_range, importance_threshold, keyword, only_created))
-        return results
-
-    def search(self, place, depth=-1, date_range=None, importance_threshold=None, keyword=None, only_created=False, fields=('text'), ranking_function=lambda data: data['importance'], max_text_len=1000):
-        raw_results = self._search(place, depth, date_range, importance_threshold, keyword, only_created)
-
-        # Sort results based on ranking function
-        raw_results.sort(key=lambda x: ranking_function(x[2]), reverse=True)
-
-        tree = {}
-        total_len = 0
-
-        def add_to_tree(path_parts, filename, contents, total_len):
-            cursor = tree
-            new_len = 0
-            for part in path_parts:
-                if part not in cursor:
-                    cursor[part] = {}
-                    new_len += len(part) + 1  # add 1 for the ':' or '/' character
-                cursor = cursor[part]
-
-            # Check if the new length will exceed the limit
-            info = {field: contents[field] for field in fields if field in contents}
-            info_len = sum(len(str(val)) for val in info.values())
-            name_len = len(filename)
-            new_len += name_len + info_len + 1  # add 1 for the ':' character
-
-            if total_len + new_len > max_text_len:
-                return total_len, False
-
-            cursor[filename] = info
-            return total_len + new_len, True
-
-        for dirpath, filename, contents in raw_results:
-            relative_path = os.path.relpath(dirpath, self.path)
-            path_parts = relative_path.split(os.path.sep)
-
-            # Try to add to the tree and update total_len
-            total_len, added = add_to_tree(path_parts, filename, contents, total_len)
-            if not added:
-                break
-
-        def pretty_print_tree(tree_dict, indent=''):
-            lines = []
-            for key, value in tree_dict.items():
-                if isinstance(value, dict):
-                    lines.append(f"{indent}{key}:")
-                    lines.extend(pretty_print_tree(value, indent=indent + '\t'))
-                else:
-                    lines.append(f"{indent}{key}: {value}")
-            return lines
-
-        lines = pretty_print_tree(tree)
-        return '\n'.join(lines)
+    def get_last(self,idx:int):
+        cur=read_last_line(join(self.history,f'event_{idx}.jsonl'))
+        cur=json.loads(cur)
+        return cur
+   
+    @staticmethod
+    def verify_log(d:Optional[Union[Dict,None]] = None):
+        if d==None: 
+            return 
+        for k in ('text','importance','viewed'):
+            assert k in d.keys()
 
 
+    def scores(self):
+        
+        relevance={}
+        importance={} 
+        fresh={}
+        entries=tuple(join(self.curent,j) for j in os.listdir(self.curent))
 
-     
-if __name__ == '__main__':
-    if exists('lol'):
-    	rmtree('lol')
+        for path in entries:
+            t=time.time()-getctime(path)
+            with open(path) as f:
+                d=json.load(f)
+                #print(d)
+            fresh[path]=t
+            relevance[path]=d['viewed']/t
+            importance[path]=d['importance'] 
 
-    x=memory.make_new('lol')
-    x.add_folder('dirr','discribing this folder')
-    x.add_memory('mem','first',1)
-    x.add_memory('mem','i added u',10,place=join('history','dirr'))
-    memory('lol').change_importance('mem',2,place=join('history','dirr'))
-    print(memory('lol')._search('history',2,importance_threshold=2))
+        relevance=sorted(entries,key=lambda k:relevance[k])
+        importance=sorted(entries,key=lambda k:importance[k])
+        fresh=sorted(entries,key=lambda k:fresh[k],reverse=True) 
 
-    print('\n\n\n')
-    x.add_memory('mem2','first',1)
-    x.add_memory('stuff','first',1)
-    x.remove_memory('mem', place=join('history','dirr'))
-    #x.remove_folder('dirr', 'history')
-    x.add_folder('check','discribing this folder',place="")
-    print(memory('lol')._search('history', 1,keyword='mem'))
+        scores={k:[None,None,None] for k in entries}
 
-    print('\n\n\n')
+        for i,x in enumerate(relevance):
+            scores[x][0]=i
+        for i,x in enumerate(importance):
+            scores[x][1]=i 
+        for i,x in enumerate(fresh):
+            scores[x][2]=i 
 
-    x.add_folder('check 2', 'history')
-    x.add_folder('check','discribing this folder',place="")
-    tree=memory('lol').search('', -1,only_created=True,max_text_len=100,fields=('text',))
-    print(tree)
-    #print(data)
+        return scores
+
+    def prune(self):
+        scores=self.scores()
+        bad=min(scores.keys(),key=lambda k:scores[k])
+        idx=int(bad[:-6].split('_')[-1])
+        self._modify(idx)
+
+    def log_history(self,idx:int,d:Optional[Union[Dict,None]] = None):
+        '''
+        takes in an event index and the curent state of that event and modifys in the change
+        if the event is new it will be created
+        '''
+        
+        with open(join(self.history,f'event_{idx}.jsonl'),'a') as f:
+            f.write('\n'+json.dumps(d)) 
+
+    def add(self,text :str,importance : Optional[int]=0):
+        if len(os.listdir(self.curent))>=self.cap: 
+            self.prune()
+
+        idx=self.get_count()
+        d={'text':text,'importance':importance,'viewed':0}
+        with open(join(self.curent,f'event_{idx}.jsonl'),'w') as f:
+            json.dump(d,f)
+        self.log_history(idx,d)
+        self._increment_count()
+
+    def _modify(self,idx:int,d:Optional[Union[Dict,None]] = None):
+        self.verify_log(d)
+        path=join(self.curent,f'event_{idx}.jsonl')
+        if d==None:
+            os.remove(path)
+        else:
+            with open(path,'w') as f:
+                json.dump(d,f)
+        
+        self.log_history(idx,d)
+
+    def get_all(self):
+        ans=[]
+        for j in os.listdir(self.curent):
+            with open(join(self.curent,j)) as f:
+                ans.append(json.load(f))
+        return ans
+
+if __name__=='__main__':
+    from shutil import rmtree
+    if exists('mem_data'):
+        rmtree('mem_data')
+    
+    a=MemoryFolder('mem_data',cap=3,new=True)
+    a.add('hey')
+    a.add('yo')
+    a.add('I am me',3)
+    a.add('bad',0)
+    a.add('bad2',0)
+    a._modify(1,{'text':'yes','importance':3,'viewed':1})
+    a._modify(0,None)
+    a.prune()
+    print(a.get_all())
